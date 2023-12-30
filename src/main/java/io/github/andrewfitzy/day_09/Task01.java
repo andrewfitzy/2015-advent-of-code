40114fc8480a5e5a15fa29c29fe4f06f68222d15
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2023-12-30 07:55:37 +0000

    day 09 skeleton

---
+++ src/main/java/io/github/andrewfitzy/day_09/Task01.java
@@ -0,0 +1,17 @@
+/* (C)2023 */
+package io.github.andrewfitzy.day_09;
+
+import java.util.List;
+
+public class Task01 {
+
+    private final List<String> fileContent;
+
+    public Task01(final List<String> fileContent) {
+        this.fileContent = fileContent.stream().toList();
+    }
+
+    public int solve() {
+        return 0;
+    }
+}
---
+++ src/test/java/io/github/andrewfitzy/day_09/TestTask02.java
@@ -0,0 +1,32 @@
+/* (C)2023 */
+package io.github.andrewfitzy.day_09;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.andrewfitzy.TaskInputReader;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class TestTask02 {
+
+    @Test
+    void demoTestMethod_01() {
+        Task01 task02 = new Task01(
+                Arrays.asList("London to Dublin = 464" + "London to Belfast = 518" + "Dublin to Belfast = 141"));
+
+        int result = task02.solve();
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testSolveWithRealData() {
+        List<String> fileContent = TaskInputReader.getFileContent("./day_09/task01_input.txt");
+
+        Task02 task02 = new Task02(fileContent);
+        int result = task02.solve();
+
+        assertEquals(0, result);
+    }
+}

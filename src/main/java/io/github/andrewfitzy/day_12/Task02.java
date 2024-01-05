75c4da3b2378ca6060d2cd358dfed9d7d11c5be7
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2024-01-05 15:34:18 +0000

    Adding day 12 pt 2

--- src/main/java/io/github/andrewfitzy/day_12/Task02.java
+++ src/main/java/io/github/andrewfitzy/day_12/Task02.java
@@ -1,10 +1,16 @@
 /* (C)2023 */
 package io.github.andrewfitzy.day_12;
 
-import java.util.List;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public final class Task02 {
-
+public class Task02 {
+    public final Pattern NUMBER_PATTERN = Pattern.compile("-?\\d+");
     private final List<String> fileContent;
 
     public Task02(final List<String> fileContent) {
@@ -12,6 +18,60 @@ public final class Task02 {
     }
 
     public int solve() {
-        return 0;
+        String input = fileContent.get(0);
+
+        StringBuilder builder = new StringBuilder();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode node = objectMapper.readTree(input);
+            processJSONTree(node);
+            builder.append(objectMapper.writeValueAsString(node));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Matcher matcher = NUMBER_PATTERN.matcher(builder.toString());
+        int total = 0;
+        while (matcher.find()) {
+            total = total + Integer.parseInt(matcher.group());
+        }
+        return total;
+    }
+
+    public void processJSONTree(JsonNode root) {
+        if (root.isObject() && nodeContainsRed(root)) {
+            ObjectNode node = (ObjectNode) root;
+            node.removeAll();
+        }
+        processNode(root);
+    }
+
+    private void processNode(JsonNode node) {
+        for (Iterator<JsonNode> nodeElements = node.elements(); nodeElements.hasNext(); ) {
+            JsonNode nodeElement = nodeElements.next();
+            boolean containsRed = nodeContainsRed(nodeElement);
+            if (containsRed) {
+                nodeElements.remove();
+            }
+        }
+
+        for (Iterator<JsonNode> nodeElements = node.elements(); nodeElements.hasNext(); ) {
+            JsonNode nodeElement = nodeElements.next();
+            processNode(nodeElement);
+        }
+    }
+
+    private boolean nodeContainsRed(JsonNode node) {
+        if (node.isObject()) {
+            for (Iterator<Map.Entry<String, JsonNode>> nodeFields = node.fields(); nodeFields.hasNext(); ) {
+                Map.Entry<String, JsonNode> entry = nodeFields.next();
+
+                JsonNode tmpNode = entry.getValue();
+                if (tmpNode.isValueNode() && "red".equals(tmpNode.asText())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/test/java/io/github/andrewfitzy/day_12/TestTask02.java
+++ src/test/java/io/github/andrewfitzy/day_12/TestTask02.java
@@ -9,14 +9,49 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TestTask02 {
-
     @Test
     void demoTestMethod_01() {
-        Task02 task02 = new Task02(Arrays.asList(""));
+        Task02 task02 = new Task02(Arrays.asList("[1,2,3]"));
+
+        int result = task02.solve();
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    void demoTestMethod_02() {
+        Task02 task02 = new Task02(Arrays.asList("[1,{\"c\":\"red\",\"b\":2},3]"));
+
+        int result = task02.solve();
+
+        assertEquals(4, result);
+    }
+
+    @Test
+    void demoTestMethod_03() {
+        Task02 task02 = new Task02(Arrays.asList("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}"));
 
         int result = task02.solve();
 
         assertEquals(0, result);
+    }
+
+    @Test
+    void demoTestMethod_04() {
+        Task02 task02 = new Task02(Arrays.asList("[1,\"red\",5]"));
+
+        int result = task02.solve();
+
+        assertEquals(6, result);
+    }
+
+    @Test
+    void demoTestMethod_05() {
+        Task02 task02 = new Task02(Arrays.asList("[1,{\"c\":\"blue\",\"a\":{\"ac\":\"red\",\"ab\":2},\"b\":2},3]"));
+
+        int result = task02.solve();
+
+        assertEquals(6, result);
     }
 
     // @Test
@@ -26,6 +61,6 @@ public class TestTask02 {
         Task02 task02 = new Task02(fileContent);
         int result = task02.solve();
 
-        assertEquals(0, result);
+        assertEquals(87842, result);
     }
 }
